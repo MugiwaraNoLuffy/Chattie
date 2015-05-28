cd75712e63c7939cc2f4e8b96ec5c28b3fbae89e
MugiwaraNoLuffy <redi.unipampa@gmail.com>
2015-05-28 12:45:43 -0700

    Test - Sending and receiving messages

--- app/src/main/java/com/example/ubuntu/parseservice/MainActivity.java
+++ app/src/main/java/com/example/ubuntu/parseservice/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.ubuntu.parseservice;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationProvider;
 import android.os.Handler;
 import android.os.ResultReceiver;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.Parse;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
+import com.parse.ParseRelation;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,12 +34,34 @@ public class MainActivity extends ActionBarActivity {
         intent = new Intent(this, ParseService.class);
         intent.putExtra("receiver", resultReceiver);
         startService(intent);
+        final EditText edit = (EditText) findViewById(R.id.message);
+        Parse.initialize(this, "zt3WZ4oUyeGcnq0ngrqBeqJE05PWeOdP6c2XYpaR", "SSLu3imS5yFuKwYQ5Zq0OW8PhkU1sbmsPbksDz0Z");
+        edit.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(MainActivity.this, edit.getText(), Toast.LENGTH_SHORT).show();
 
+                    ParseObject obj = new ParseObject("msg_chatest");
+                    obj.put("userid", ParseObject.createWithoutData("Users", "7hKOJBhn7J"));
+                    obj.put("message", edit.getText().toString());
+                    obj.saveInBackground();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
-    public void onDestroy(){
+    public void onStop(){
         stopService(intent);
+        super.onStop();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
     }
 
     @Override
@@ -74,7 +105,8 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected void onReceiveResult(int resultCode, Bundle resultData)
         {
-            runOnUiThread(new UpdateUI("Result Received " + (String)((ParseService.Teste)resultData.getSerializable("lista")).get().getString("username")));
+            if(resultData != null)
+                runOnUiThread(new UpdateUI((String)((ParseService.Teste)resultData.getSerializable("lista")).get()));
         }
     }
 }
--- app/src/main/java/com/example/ubuntu/parseservice/ParseService.java
+++ app/src/main/java/com/example/ubuntu/parseservice/ParseService.java
@@ -9,10 +9,12 @@ import android.util.Log;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -38,15 +40,15 @@ public class ParseService extends Service {
 
         timerTask = new MyTimerTask();
         timer.scheduleAtFixedRate(timerTask, 5000, 5000);
-        ParseObject testObject = new ParseObject("Users");
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Users");
+        ParseObject testObject = new ParseObject("msg_chatest");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("msg_chatest");
         //query.whereEqualTo("playerName", "Dan Stemkoski");
-
+        query.include("userid");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, com.parse.ParseException e) {
                 if (e == null) {
-                    Log.d("score", "Retrieved " + list.size() + " scores");
+                    Log.d("score", "Retrieved " + list.size() + " messages");
                     Bundle bundle = new Bundle();
                     Teste teste = new Teste(list);
                     bundle.putSerializable("lista", teste);
@@ -61,13 +63,14 @@ public class ParseService extends Service {
     }
 
 
-
-    public void onDestroy() {
+    @Override
+    public void onDestroy()
+    {
         super.onDestroy();
         timer.cancel();
-        Bundle bundle = new Bundle();
-        bundle.putString("end", "Timer Stopped....");
-        resultReceiver.send(200, bundle);
+        //Bundle bundle = new Bundle();
+        //bundle.putString("end", "Timer Stopped....");
+        //resultReceiver.send(200, bundle);
     }
     public class Teste implements Serializable {
         List<ParseObject> objects;
@@ -76,15 +79,24 @@ public class ParseService extends Service {
             objects = parseObjects;
         }
 
-        public ParseObject get(){
-            if(objects.size() > 0)
-                return (ParseObject)objects.get(objects.size()-1);
-            return null;
+        public String get() {
+            StringBuffer stringBuffer = new StringBuffer("");
+            for( ParseObject object : objects) {
+                stringBuffer.append(object.getParseObject("userid").getString("username")+": ");
+                stringBuffer.append(object.getString("message"));
+                stringBuffer.append("\n");
+            }
+
+            return stringBuffer.toString();
          }
     }
 
     private class MyTimerTask extends TimerTask {
+        ParseObject testObject;
+        ParseQuery<ParseObject> query;
+
         public MyTimerTask() {
+            testObject = new ParseObject("msg_chatest");
             Bundle bundle = new Bundle();
             bundle.putString("start", "Timer Started....");
             //resultReceiver.send(100, bundle);
@@ -92,6 +104,31 @@ public class ParseService extends Service {
         @Override
         public void run() {
 
+            query = ParseQuery.getQuery("msg_chatest");
+            //query.whereEqualTo("playerName", "Dan Stemkoski");
+            Date date = new Date(new Date().getTime()-5000);
+            java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+            String data = sqlDate.toString();
+            Log.d("Service", data);
+            query.whereGreaterThan("updatedAt", sqlDate);
+            query.include("userid");
+
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> list, com.parse.ParseException e) throws NullPointerException {
+                    if (e == null) {
+                        Log.d("score", "Retrieved " + list.size() + " scores");
+                        Bundle bundle = new Bundle();
+                        Teste teste = new Teste(list);
+                        bundle.putSerializable("lista", teste);
+                        resultReceiver.send(100, bundle);
+
+
+                    } else {
+                        Log.d("score", "Error: " + e.getMessage());
+                    }
+                }
+            });
             //resultReceiver.send(, null);
         }
     }
